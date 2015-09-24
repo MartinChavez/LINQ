939df2a5e6be4b4fdba3d29fd3314f4571c69008
MartinChavez <martin.chavez@live.com>
2015-09-23 20:46:24 -0700

    Creating finished

--- LINQ - TestDriven Learning/LINQ Examples/Creating.cs
+++ LINQ - TestDriven Learning/LINQ Examples/Creating.cs
@@ -9,13 +9,13 @@ namespace LINQ_Examples
     {
         /* Creating */
         // It can be achieved by using the IEnumerable static class methods: Range and Repeat
-        // They are not extension methods and they are nor deferred
+        // They are not extension methods and they do not run in a 'deferred execution' manner
         [TestMethod]
-        public void CreatingUsingRange()
+        public void CreatingSequenceUsingRange()
         {
             /* Range */
             // Generates a sequence of integral numbers within a specified range
-            var integers = Enumerable.Range(0,10);//This statements will create a sequence of 10 elements 0..9
+            var integers = Enumerable.Range(0,10);//This statement will create a sequence of 10 elements 0..9
 
             var currentNumber = 0;
             foreach (var integer in integers)
@@ -32,6 +32,21 @@ namespace LINQ_Examples
             {
                 Assert.AreEqual(integer, currentNumberSelect);
                 currentNumberSelect += 2;
+            }
+        }
+
+        [TestMethod]
+        public void CreatingSequenceUsingRepeat()
+        {
+            /* Repeat */
+            // Creates a sequence of one value repeated 'n' times
+            // The Repeat() method can replicate integers and any object
+            var integers = Enumerable.Repeat(5, 10);
+
+            const int repeatedNumber = 5;
+            foreach (var integer in integers)
+            {
+                Assert.AreEqual(integer, repeatedNumber);
             }
         }
     }
